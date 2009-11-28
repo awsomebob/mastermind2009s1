6b722398e69ffcc7dc861c2038ddec10301eb7ac
nick.veenhof <nick.veenhof@abd0611c-c349-11de-896c-518660fad6c5>
2009-11-28 12:43:09 +0000

    changed

--- trunk/src/com/masprop/cluster1/privateclasses/model/Game.java
+++ trunk/src/com/masprop/cluster1/privateclasses/model/Game.java
@@ -3,7 +3,7 @@ package com.masprop.cluster1.privateclasses.model;
 /**
  *  This class represents game and contents top 10 scores for current level of difficulty
  *  and current level type.
- *  
+ *
  *  @see MastermindFileParser
  *  @see Scores
  *  @author Milan Stanic
@@ -13,64 +13,64 @@ import com.masprop.cluster1.privateclasses.model.diskmanager.MastermindFileParse
 import com.masprop.cluster1.shared.model.GameLevelType;
 
 public abstract class Game {
-	/**
-	 * Top 10 scores for current level of difficulty.
-	 */
-	private Scores scores;
-	
+    /**
+     * Top 10 scores for current level of difficulty.
+     */
+    private Scores scores;
+
     /**
      * Game difficulty level.
      */
-	private GameLevelType gameLevelType;
-	
-	/**
-	 * Game parser 
-	 * Nick will describe functions for him
-	 */
-	private MastermindFileParser parser;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param gameLevelType game difficulty level
-	 */
-	public Game(GameLevelType gameLevelType){
-		this.gameLevelType = gameLevelType;
-		parser = new MastermindFileParser("fileparser");
-		scores = new Scores(parser, gameLevelType);
-	}
-	
-	/**
-	 * 
-	 * @return top 10 scores for current difficulty level
-	 */
-	public Scores getScores() {
-		return scores;
-	}
+    private GameLevelType gameLevelType;
 
-	/**
-	 * 
-	 * @param scores top 10 scores for current difficulty level 
-	 */
-	public void setScores(Scores scores) {
-		this.scores = scores;
-	}
+    /**
+     * Game parser
+     * Nick will describe functions for him
+     */
+    private MastermindFileParser parser;
 
-	/**
-	 * 
-	 * @return game difficulty level
-	 */
-	public GameLevelType getGameLevelType() {
-		return gameLevelType;
-	}
+    /**
+     * Constructor to make a new game
+     *
+     * @param gameLevelType game difficulty level
+     */
+    public Game(GameLevelType gameLevelType){
+        this.gameLevelType = gameLevelType;
+        //parser = new MastermindFileParser("fileparser");
+        //scores = new Scores(parser, gameLevelType);
+    }
 
-	/**
-	 * 
-	 * @param gameLevelType game difficulty level
-	 */
-	public void setGameLevelType(GameLevelType gameLevelType) {
-		this.gameLevelType = gameLevelType;
-	}
-	
-	
+    /**
+     *
+     * @return top 10 scores for current difficulty level
+     */
+    public Scores getScores() {
+        return scores;
+    }
+
+    /**
+     *
+     * @param scores top 10 scores for current difficulty level
+     */
+    public void setScores(Scores scores) {
+        this.scores = scores;
+    }
+
+    /**
+     *
+     * @return game difficulty level
+     */
+    public GameLevelType getGameLevelType() {
+        return gameLevelType;
+    }
+
+    /**
+     *
+     * @param gameLevelType game difficulty level
+     */
+    public void setGameLevelType(GameLevelType gameLevelType) {
+        this.gameLevelType = gameLevelType;
+    }
+
+
 }
--- trunk/src/com/masprop/cluster1/privateclasses/model/Mastermind.java
+++ trunk/src/com/masprop/cluster1/privateclasses/model/Mastermind.java
@@ -4,9 +4,9 @@ package com.masprop.cluster1.privateclasses.model;
  * Class represents game with a current game and all necessary
  * information for generating, solving
  * or for handling the score of this game.
- * 
+ *
  * @see MastermindStatus
- * 
+ *
  * @author Milan Stanic
  */
 
@@ -14,38 +14,38 @@ import com.masprop.cluster1.shared.model.GameLevelType;
 
 
 public class Mastermind extends Game {
-	
-	/**
-	 * pile will describe this class
-	 */
-	private MastermindStatus mastermindStatus;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param gameLevelType game difficulty level
-	 */
-	public Mastermind(GameLevelType gameLevelType) {
-			super(gameLevelType);
-			mastermindStatus = new MastermindStatus();
-	}
 
-	/**
-	 * 
-	 * @return mastermindStaus
-	 */
-	public MastermindStatus getMastermindStatus() {
-		return mastermindStatus;
-	}
+    /**
+     * pile will describe this class
+     */
+    private MastermindStatus mastermindStatus;
 
-	/**
-	 * 
-	 * @param mastermindStatus status of current game
-	 */
-	public void setMastermindStatus(MastermindStatus mastermindStatus) {
-		this.mastermindStatus = mastermindStatus;
-	}
-	
-	
+    /**
+     * Constructor
+     *
+     * @param gameLevelType game difficulty level
+     */
+    public Mastermind(GameLevelType gameLevelType) {
+            super(gameLevelType);
+            mastermindStatus = new MastermindStatus();
+    }
+
+    /**
+     *
+     * @return mastermindStaus
+     */
+    public MastermindStatus getMastermindStatus() {
+        return mastermindStatus;
+    }
+
+    /**
+     *
+     * @param mastermindStatus status of current game
+     */
+    public void setMastermindStatus(MastermindStatus mastermindStatus) {
+        this.mastermindStatus = mastermindStatus;
+    }
+
+
 
 }

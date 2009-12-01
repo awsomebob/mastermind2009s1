44b96166ed45c1019ba159cd2981b675c0d2420c
nick.veenhof <nick.veenhof@abd0611c-c349-11de-896c-518660fad6c5>
2009-12-01 21:29:52 +0000

    more changes to scores and files

--- src/com/masprop/cluster1/Main.java
+++ src/com/masprop/cluster1/Main.java
@@ -6,7 +6,7 @@ import java.io.Console;
 import java.io.File;
 import com.masprop.cluster1.privateclasses.controller.*;
 import com.masprop.cluster1.privateclasses.model.*;
-import com.masprop.cluster1.privateclasses.model.diskmanager.MastermindFileParser;
+import com.masprop.cluster1.privateclasses.model.diskmanager.*;
 import com.masprop.cluster1.privateclasses.view.*;
 import com.masprop.cluster1.shared.controller.*;
 import com.masprop.cluster1.shared.model.*;
@@ -21,24 +21,27 @@ public class Main {
     public void run() {
         // Initialize Database abstraction layers
         // Initialize our scores file
-        MastermindFileParser file = new MastermindFileParser("MastermindScores.csv");
+        MastermindScoreFileParser file = new MastermindScoreFileParser();
         //put some content in our file
         //rownumbers go from 1 to ...
-        /*String[] scores = {"milan","8","difficult"};
-        file.update(0, scores);
-        String[] scores1 = {"nick","6","easy"};
-        file.update(1, scores1);
 
-        String[] scores2 = {"pile","10","difficult"};
-        file.update(0, scores2);
+        file.update(GameLevelType.EASY.toString(), new Score("milan", 8));
+        file.update(GameLevelType.EASY.toString(), new Score("nick", 20));
+        file.update(GameLevelType.EASY.toString(), new Score("pile",10));
+
+        Scores scores = new Scores(file.getScoreFromFile(GameLevelType.EASY));
+
+        for(Score s : scores.getScores()){
+            System.out.print("Some scores");
+            System.out.print(s.toString());
+        }
+
         //get the content in our file as a list of stringarrays
-*/
-
         // Instantiate the Models
-        GameManagerMastermind manager =  new GameManagerMastermind();
+        //GameManagerMastermind manager =  new GameManagerMastermind();
 
 
-        Mastermind mastermind = manager.getNewGame(GameLevelType.DIFFICULT);
+        //Mastermind mastermind = manager.getNewGame(GameLevelType.DIFFICULT);
         //MastermindStatus status = mastermind.getMastermindStatus();
         //MatrixMastermind matrix = status.getMatrixMastermind();
         //System.out.println("matrixwidth");

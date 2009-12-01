e0e36b54867c0d690c85e30e95cf066d370d1769
nick.veenhof <nick.veenhof@abd0611c-c349-11de-896c-518660fad6c5>
2009-12-01 23:45:52 +0000

    scores working

--- trunk/src/com/masprop/cluster1/privateclasses/tests/ScoresTest.java
+++ trunk/src/com/masprop/cluster1/privateclasses/tests/ScoresTest.java
@@ -1,6 +1,7 @@
 package com.masprop.cluster1.privateclasses.tests;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.masprop.cluster1.privateclasses.model.Score;
 import com.masprop.cluster1.privateclasses.model.Scores;
@@ -32,11 +33,12 @@ public class ScoresTest {
         /**
          * initiate array temp with some values
          */
-        for(int i=0; i<10; i++) {
-            test.scores.addScore(new Score("test",i*3));
+        for(int i=0; i<20; i++) {
+            Random r = new Random();
+            Score score = new Score("test",r.nextInt(100));
+            test.scores.addScore(score);
         }
-        //sort our scores
-        test.scores.sortScores();
+
 
         /**
          * write top 10 scores to standard output
@@ -50,7 +52,7 @@ public class ScoresTest {
          * current position of score in top 10 or -1 if that score is not in top 10
          * this variable score you can change to test functionality of class Score
          */
-        int score = 66;
+        int score = 13;
         String name = "testname";
         int position = test.scores.getScore(score);
         System.out.println();

2515d214c51d3e4a5760649aa1f7141225f73952
nick.veenhof <nick.veenhof@abd0611c-c349-11de-896c-518660fad6c5>
2009-12-01 23:45:52 +0000

    scores working

--- src/com/masprop/cluster1/privateclasses/model/Score.java
+++ src/com/masprop/cluster1/privateclasses/model/Score.java
@@ -3,15 +3,16 @@ package com.masprop.cluster1.privateclasses.model;
 /*
  * @author Nick
  */
-public class Score {
+public class Score implements Comparable<Score> {
     private int score;
+
     private String name;
 
     public Score() {
-        //default constructor but is not used
+        // default constructor but is not used
     }
 
-    public Score(String name,int score) {
+    public Score(String name, int score) {
         this.setName(name);
         this.setScore(score);
     }
@@ -23,9 +24,11 @@ public class Score {
     public int getScore() {
         return score;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setScore(int score) {
         this.score = score;
     }
@@ -37,10 +40,18 @@ public class Score {
 
     @Override
     public String toString() {
-        return getName()+"|"+getScore();
+        return getName() + "|" + getScore();
     }
 
     public String toString(String separator) {
-        return getName()+separator+getScore();
+        return getName() + separator + getScore();
+    }
+
+    public int compareTo(Score score) {
+        if ((this.getScore() > score.getScore())) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
--- src/com/masprop/cluster1/privateclasses/model/Scores.java
+++ src/com/masprop/cluster1/privateclasses/model/Scores.java
@@ -1,92 +1,101 @@
 package com.masprop.cluster1.privateclasses.model;
+
 import com.masprop.cluster1.privateclasses.model.diskmanager.MastermindScoreFileParser;
 import com.masprop.cluster1.shared.model.GameLevelType;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
- *This class contents the best 10 scores for appropriate level of difficulty
+ * This class contents the best 10 scores for appropriate level of difficulty
  *
- *@author Milan Stanic
-*/
+ * @author Milan Stanic
+ */
 public class Scores {
 
     /**
      * top 10 results for appropriate level
      */
+    private static int LIMIT = 10;
+
     private ArrayList<Score> scores;
 
     /**
-     * Class constructor
-     * purpose for testing
+     * Class constructor purpose for testing
      */
-    public Scores(){
-        for(int i=0; i<10; i++) {
-            scores.add(new Score("Name",5));
-        }
+    public Scores() {
+        this.scores = new ArrayList<Score>();
     }
 
-    public Scores(ArrayList<Score> scores){
+    public Scores(ArrayList<Score> scores) {
         this.setScores(scores);
     }
+
     /**
      * Class constructor.
      *
-     * @param gameLevelType difficulty level
-     * @param parser returns array of the best 10 results
+     * @param gameLevelType
+     *            difficulty level
+     * @param parser
+     *            returns array of the best 10 results
      */
-    public Scores(MastermindScoreFileParser parser, GameLevelType gameLevelType){
+    public Scores(MastermindScoreFileParser parser, GameLevelType gameLevelType) {
         // in parser we need to have method which reads appropriate
         // file with score and returns array of 10 the best result
         scores = parser.getScoreFromFile(gameLevelType);
     }
 
-
-
-
     /**
-     * @param score score of current game
-     * @return current position of score in top 10 or -1 if that score is not in top 10
+     * @param score
+     *            score of current game
+     * @return current position of score in top 10 or -1 if that score is not in
+     *         top 10
      */
-    public int getScore(int score){
-        //our value should atleast have a count greater then the size-10;
-        //if the size is
+    public int getScore(int score) {
         int position = -1;
-        for(Score s : this.scores) {
-            if(score > s.getScore()){
-                //put it in the arraylist in this position and then quit the foreach
+        for (Score s : this.scores) {
+            if (score < s.getScore()) {
+                // put it in the arraylist in this position and then quit the
+                // foreach
                 position = scores.indexOf(s);
                 break;
             }
         }
-        /*int temp = 1;
-        for(temp=1; temp<=10; temp++){
-            if(score < scores[9]){ temp = -1; break; }
-            if(score > scores[temp-1]) break;
-        }*/
+        /*
+         * int temp = 1; for(temp=1; temp<=10; temp++){ if(score < scores[9]){
+         * temp = -1; break; } if(score > scores[temp-1]) break; }
+         */
         return position;
     }
 
     /**
-     * @param score score of current game
-     * @param position current position for score
-    */
-    public void setScore(Score score, int position){
+     * @param score
+     *            score of current game
+     * @param position
+     *            current position for score
+     */
+    public void setScore(Score score, int position) {
         scores.add(position, score);
-        /*for(int i=9; i>=position; i--){
-            scores[i] = scores[i-1];
-        }
-        scores[position-1] = score;*/
+        /*
+         * for(int i=9; i>=position; i--){ scores[i] = scores[i-1]; }
+         * scores[position-1] = score;
+         */
     }
 
     /**
-     * @param score score of current game
-    */
-    public void addScore(Score score){
-        scores.add(score);
-
-        /*for(int i=9; i>=position; i--){
-            scores[i] = scores[i-1];
+     * @param score
+     *            score of current game
+     */
+    public void addScore(Score score) {
+        // check if the object is a score
+        if ((score.getName() != null) && (score.getScore() != 0)) {
+            this.scores.add(score);
         }
-        scores[position-1] = score;*/
+        this.sortScores();
+        this.cutToLimit(LIMIT);
+        /*
+         * for(int i=9; i>=position; i--){ scores[i] = scores[i-1]; }
+         * scores[position-1] = score;
+         */
     }
 
     /**
@@ -99,7 +108,8 @@ public class Scores {
 
     /**
      *
-     * @param scores top 10 scores for current level
+     * @param scores
+     *            top 10 scores for current level
      */
     public void setScores(ArrayList<Score> scores) {
         this.scores = scores;
@@ -110,7 +120,23 @@ public class Scores {
      *
      */
     public void sortScores() {
-        //sort the arraylist of scores
+        Object[] arrScores = scores.toArray();
+        Arrays.sort(arrScores);
+        ArrayList<Score> scores = new ArrayList<Score>();
+        for (Object o : arrScores) {
+            // convert to score
+            Score score = (Score) o;
+            scores.add(score);
+        }
+        this.scores = scores;
+        // sort the arraylist of scores
     }
 
+    public void cutToLimit(int max) {
+        if (this.scores.size() >= max) {
+            for (int i = max-1; i < this.scores.size(); i++) {
+                this.scores.remove(i);
+            }
+        }
+    }
 }

482d0ae564c3c68a7a1912773712fd6e7eec1b95
nick.veenhof <nick.veenhof@abd0611c-c349-11de-896c-518660fad6c5>
2010-01-13 20:25:22 +0000

    about and manual dialogs

--- trunk/src/com/masprop/cluster1/privateclasses/view/AboutDialog.java
+++ trunk/src/com/masprop/cluster1/privateclasses/view/AboutDialog.java
@@ -15,7 +15,7 @@ import javax.swing.JTextArea;
 
 /**
  * Dialog about game
- * 
+ *
  * @author Milan Stanic
  *
  */
@@ -24,106 +24,106 @@ import javax.swing.JTextArea;
 
 public class AboutDialog extends JDialog {
 
-	
-	private static final long serialVersionUID = 1L;
-	private JPanel jContentPane = null;
-	
-	private JButton jButton = null;
 
-	private JTextArea jTextArea = null;
-	
-	private MyGUI myGUI;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param owner
-	 */
-	public AboutDialog(Frame owner){
-		super(owner);
-		initialize();
-	}
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param owner
-	 */
-	public AboutDialog(MyGUI owner){
-		super(owner);
-		myGUI = owner;
-		initialize();
-	}
-	
-	/**
-	 * This method initializes this
-	 * 
-	 * @return void
-	 */
-	private void initialize() {
-		this.setSize(200, 210);
-		this.setTitle("About Game");
-		this.setContentPane(getJContentPane());
-	}
-	
-	/**
-	 * This method initializes jContentPane
-	 * 
-	 * @return javax.swing.JPanel
-	 */
-	private JPanel getJContentPane() {
-		if (jContentPane == null) {
-			
-			
+    private static final long serialVersionUID = 1L;
+    private JPanel jContentPane = null;
 
-			
-			jTextArea = new JTextArea();
-			jTextArea.setBounds(new Rectangle(20, 20, 150, 100));
-			jTextArea.setText("player");
-			jTextArea.setEditable(false);
-			jTextArea.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
-			
-			jContentPane = new JPanel();
-			jContentPane.setLayout(null);
-			jContentPane.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
-			
-			
-			jContentPane.add(jTextArea, null);
-			
-			jContentPane.add(getJButton(), null);
-						
-		}
-		return jContentPane;
-	}
-	
-	/**
-	 * This method initializes jButton	
-	 * 	
-	 * @return javax.swing.JButton	
-	 */
-	private JButton getJButton() {
-		if (jButton == null) {
-			jButton = new JButton();
-			jButton.setBounds(new Rectangle(20, 145, 140, 20));
-			jButton.setText("Cancel");
-			jButton.addActionListener(new java.awt.event.ActionListener() {
-	             public void actionPerformed(java.awt.event.ActionEvent evt) {
-	            	 cancelEventActionPerformed(evt);
-	             }
-	         });
-		}
-		return jButton;
-	}
+    private JButton jButton = null;
+
+    private JTextArea jTextArea = null;
+
+    private MyGUI myGUI;
+
+    /**
+     * Constructor
+     *
+     * @param owner
+     */
+    public AboutDialog(Frame owner){
+        super(owner);
+        initialize();
+    }
+
+    /**
+     * Constructor
+     *
+     * @param owner
+     */
+    public AboutDialog(MyGUI owner){
+        super(owner);
+        myGUI = owner;
+        initialize();
+    }
+
+    /**
+     * This method initializes this
+     *
+     * @return void
+     */
+    private void initialize() {
+        this.setSize(250, 260);
+        this.setTitle("About Game");
+        this.setContentPane(getJContentPane());
+        this.setResizable(false);
+    }
+
+    /**
+     * This method initializes jContentPane
+     *
+     * @return javax.swing.JPanel
+     */
+    private JPanel getJContentPane() {
+        if (jContentPane == null) {
+
+            jTextArea = new JTextArea();
+            jTextArea.setBounds(new Rectangle(20, 20, 200, 140));
+            jTextArea.setText("This game was created for learning purposes. It is licensed under a GPLv2 agreement. If you have any questions about this program please email us on the following address: nick.veenhof@gmail.com");
+            jTextArea.setEditable(false);
+            jTextArea.setLineWrap(true);
+            jTextArea.setWrapStyleWord(true);
+            jTextArea.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
+
+            jContentPane = new JPanel();
+            jContentPane.setLayout(null);
+            jContentPane.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
 
 
+            jContentPane.add(jTextArea, null);
 
-	/**
-	 * Closing dialog
-	 * @param evt
-	 */
-	private void cancelEventActionPerformed(java.awt.event.ActionEvent evt){//GEN-FIRST:event_newGameMenuItemActionPerformed
-    		
-		this.setVisible(false);	
+            jContentPane.add(getJButton(), null);
+
+        }
+        return jContentPane;
+    }
+
+    /**
+     * This method initializes jButton
+     *
+     * @return javax.swing.JButton
+     */
+    private JButton getJButton() {
+        if (jButton == null) {
+            jButton = new JButton();
+            jButton.setBounds(new Rectangle(20, 180, 140, 20));
+            jButton.setText("Cancel");
+            jButton.addActionListener(new java.awt.event.ActionListener() {
+                 public void actionPerformed(java.awt.event.ActionEvent evt) {
+                     cancelEventActionPerformed(evt);
+                 }
+             });
+        }
+        return jButton;
+    }
+
+
+
+    /**
+     * Closing dialog
+     * @param evt
+     */
+    private void cancelEventActionPerformed(java.awt.event.ActionEvent evt){//GEN-FIRST:event_newGameMenuItemActionPerformed
+
+        this.setVisible(false);
     }
 
 }
--- trunk/src/com/masprop/cluster1/privateclasses/view/ManualDialog.java
+++ trunk/src/com/masprop/cluster1/privateclasses/view/ManualDialog.java
@@ -11,111 +11,111 @@ import javax.swing.JTextArea;
 
 /**
  * Dialog about manual
- * 
+ *
  * @author Milan Stanic
  *
  */
 
 public class ManualDialog extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel jContentPane = null;
-	
-	private JButton jButton = null;
+    private static final long serialVersionUID = 1L;
+    private JPanel jContentPane = null;
 
-	private JTextArea jTextArea = null;
-	
-	private MyGUI myGUI;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param owner
-	 */
-	public ManualDialog(Frame owner){
-		super(owner);
-		initialize();
-	}
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param owner
-	 */
-	public ManualDialog(MyGUI owner){
-		super(owner);
-		myGUI = owner;
-		initialize();
-	}
-	
-	/**
-	 * This method initializes this
-	 * 
-	 * @return void
-	 */
-	private void initialize() {
-		this.setSize(200, 210);
-		this.setTitle("Manual for Game");
-		this.setContentPane(getJContentPane());
-	}
-	
-	/**
-	 * This method initializes jContentPane
-	 * 
-	 * @return javax.swing.JPanel
-	 */
-	private JPanel getJContentPane() {
-		if (jContentPane == null) {
-			
-			
+    private JButton jButton = null;
 
-			
-			jTextArea = new JTextArea();
-			jTextArea.setBounds(new Rectangle(20, 20, 150, 100));
-			jTextArea.setText("player");
-			jTextArea.setEditable(false);
-			jTextArea.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
-			
-			jContentPane = new JPanel();
-			jContentPane.setLayout(null);
-			jContentPane.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
-			
-			
-			jContentPane.add(jTextArea, null);
-			
-			jContentPane.add(getJButton(), null);
-						
-		}
-		return jContentPane;
-	}
-	
-	/**
-	 * This method initializes jButton	
-	 * 	
-	 * @return javax.swing.JButton	
-	 */
-	private JButton getJButton() {
-		if (jButton == null) {
-			jButton = new JButton();
-			jButton.setBounds(new Rectangle(20, 145, 140, 20));
-			jButton.setText("Cancel");
-			jButton.addActionListener(new java.awt.event.ActionListener() {
-	             public void actionPerformed(java.awt.event.ActionEvent evt) {
-	            	 cancelEventActionPerformed(evt);
-	             }
-	         });
-		}
-		return jButton;
-	}
+    private JTextArea jTextArea = null;
+
+    private MyGUI myGUI;
+
+    /**
+     * Constructor
+     *
+     * @param owner
+     */
+    public ManualDialog(Frame owner){
+        super(owner);
+        initialize();
+    }
+
+    /**
+     * Constructor
+     *
+     * @param owner
+     */
+    public ManualDialog(MyGUI owner){
+        super(owner);
+        myGUI = owner;
+        initialize();
+    }
+
+    /**
+     * This method initializes this
+     *
+     * @return void
+     */
+    private void initialize() {
+        this.setSize(250, 260);
+        this.setTitle("Manual for Game");
+        this.setContentPane(getJContentPane());
+        this.setResizable(false);
+    }
+
+    /**
+     * This method initializes jContentPane
+     *
+     * @return javax.swing.JPanel
+     */
+    private JPanel getJContentPane() {
+        if (jContentPane == null) {
+
+            jTextArea = new JTextArea();
+            jTextArea.setBounds(new Rectangle(20, 20, 200, 140));
+            jTextArea.setText("Click on File, New Game to start a new game. If you really don't know what to do please consult the manual included with this game. It situates itself in the same folder as where the game is installed.");
+            jTextArea.setEditable(false);
+            jTextArea.setLineWrap(true);
+            jTextArea.setWrapStyleWord(true);
+            jTextArea.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
+
+            jContentPane = new JPanel();
+            jContentPane.setLayout(null);
+            jContentPane.setBackground(new Color(Integer.valueOf("f3a15a", 16)));
 
 
+            jContentPane.add(jTextArea, null);
 
-	/**
-	 * Closing dialog
-	 * @param evt
-	 */
-	private void cancelEventActionPerformed(java.awt.event.ActionEvent evt){//GEN-FIRST:event_newGameMenuItemActionPerformed
-    		
-		this.setVisible(false);	
+            jContentPane.add(getJButton(), null);
+
+        }
+        return jContentPane;
+    }
+
+    /**
+     * This method initializes jButton
+     *
+     * @return javax.swing.JButton
+     */
+    private JButton getJButton() {
+        if (jButton == null) {
+            jButton = new JButton();
+            jButton.setBounds(new Rectangle(20, 180, 140, 20));
+            jButton.setText("Cancel");
+            jButton.addActionListener(new java.awt.event.ActionListener() {
+                 public void actionPerformed(java.awt.event.ActionEvent evt) {
+                     cancelEventActionPerformed(evt);
+                 }
+             });
+        }
+        return jButton;
+    }
+
+
+
+    /**
+     * Closing dialog
+     * @param evt
+     */
+    private void cancelEventActionPerformed(java.awt.event.ActionEvent evt){//GEN-FIRST:event_newGameMenuItemActionPerformed
+
+        this.setVisible(false);
     }
 }
